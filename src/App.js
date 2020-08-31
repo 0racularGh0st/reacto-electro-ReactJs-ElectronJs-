c100dac6404e72e29f947f192a7a1d7fda3c1883
0racularGh0st <nigel4tariang@gmail.com>
2020-08-31 14:57:44 +0530

    Edit app.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,14 @@ export default class App extends React.Component {
     }
     render(){
         return(
-            <div>
-            Edit src/App.js to make changes
+            <div style={{textAlign:'center'}}>
+                ReactJs + ElectronJs :  A starter project for a Cross Platform app (Windows,Linux,MacOS)
+                <br/>
+
+                 Edit src/App.js to make changes
+
+                 <br/>
+                 To export your app as an executable, run command 'npm run ebuild' and files will be in the dist folder. 
             </div>
         );
     }
